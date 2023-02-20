6c9d92cc141b9a6773f1d9d8956b21fccdb48569
jesterb0206 <jesterb@seattleu.edu>
2023-02-19 22:18:01 -0800

    Analyzing the Performance of Arrays and Objects

---
+++ Analyzing-the-Performance-of-Arrays-and-Objects/big-o-array-methods.js
@@ -0,0 +1,33 @@
+// ARRAYS
+
+// Ordered lists!
+
+let names = ['Michael', 'Melissa', 'Andrea'];
+
+let values = [true, {}, [], 2, 'awesome'];
+
+// When To Use Arrays
+
+// When you need order
+// When you need fast
+// access / insertion and
+// removal (sort of ...)
+
+// Big O of Arrays
+
+// Insertion - It depends ...
+// Removal - It depends ...
+// Searching - O(N)
+// Access - O(1)
+
+// Big O of Array Operations
+
+// push - O(1)
+// pop - O(1)
+// shift - O(N)
+// unshift - O(N)
+// concat - O(N)
+// slice - O(N)
+// splice - O(N)
+// sort - O(N * log N)
+// forEach/map/filter/reduce/etc. - O(N)
---
+++ Analyzing-the-Performance-of-Arrays-and-Objects/big-o-objects.js
@@ -0,0 +1,32 @@
+// OBJECTS
+
+// Unordered, key value pairs!
+
+let instructor = {
+  firstName: 'Kelly',
+  isInstructor: true,
+  favoriteNumbers: [1, 2, 3, 4],
+};
+
+// When to use objects
+
+// When you don't need order
+// When you need fast access / insertion
+// and removal
+
+// Big O of Objects
+
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(N)
+// Access - O(1)
+
+// When you don't need any ordering,
+// objects are an excellent choice!
+
+// Big O of Object Methods
+
+// Object.keys - O(N)
+// Object.values - O(N)
+// Object.entries - O(N)
+// hasOwnProperty - O(1)
